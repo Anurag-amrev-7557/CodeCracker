65d8b754303b566d9e78ac4419390cd36c789d6b
Anurag-amrev7557 <mindreaders7557@gmail.com>
2025-01-11 20:15:11 +0530

    changes

--- src/components/KeyFeatures.jsx
+++ src/components/KeyFeatures.jsx
@@ -82,44 +82,47 @@ function KeyFeatures() {
     
 
   return (
-    <section className="keyfeatures" id='particles-js'>
-        <div className="feature">
-            <div className="feature-content">
-                <h2>Progress Tracking</h2>
-                <p>Easily track your coding journey with our intuitive dashboard. Monitor problems solved, languages used, and improvement over time.</p>
+    <>
+        <h2 className='KeyFeatures-heading'>What Makes Us Stand Out?</h2>
+        <section className="keyfeatures" id='particles-js'>
+            <div className="feature">
+                <div className="feature-content">
+                    <h2>Progress Tracking</h2>
+                    <p>Easily track your coding journey with our intuitive dashboard. Monitor problems solved, languages used, and improvement over time.</p>
+                </div>
+                <div className="feature-image">
+                    <img src={feature1} alt="Feature" />
+                </div>
             </div>
+            <div className="feature other">
             <div className="feature-image">
-                <img src={feature1} alt="Feature" />
+                    <img src={feature2} alt="Feature" />
+                </div>
+                <div className="feature-content">
+                    <h2>Analytics Dashboard</h2>
+                    <p>Get detailed analytics on your performance with insights into problem-solving speed, topic strengths, and areas for improvement.</p>
+                </div>
             </div>
-        </div>
-        <div className="feature other">
-        <div className="feature-image">
-                <img src={feature2} alt="Feature" />
+            <div className="feature">
+                <div className="feature-content">
+                    <h2>Goal Setting</h2>
+                    <p>Set daily, weekly, or monthly coding goals and stay motivated. Our reminders and achievement badges will keep you on track!</p>
+                </div>
+                <div className="feature-image">
+                    <img src={feature3} alt="Feature" />
+                </div>
             </div>
-            <div className="feature-content">
-                <h2>Analytics Dashboard</h2>
-                <p>Get detailed analytics on your performance with insights into problem-solving speed, topic strengths, and areas for improvement.</p>
-            </div>
-        </div>
-        <div className="feature">
-            <div className="feature-content">
-                <h2>Goal Setting</h2>
-                <p>Set daily, weekly, or monthly coding goals and stay motivated. Our reminders and achievement badges will keep you on track!</p>
-            </div>
+            <div className="feature other">
             <div className="feature-image">
-                <img src={feature3} alt="Feature" />
+                    <img src={feature4} alt="Feature" />
+                </div>
+                <div className="feature-content">
+                    <h2>Community Comparisons</h2>
+                    <p>See how you rank among your peers on popular platforms like Codeforces, LeetCode, and HackerRank. Compete and improve together!</p>
+                </div>
             </div>
-        </div>
-        <div className="feature other">
-        <div className="feature-image">
-                <img src={feature4} alt="Feature" />
-            </div>
-            <div className="feature-content">
-                <h2>Community Comparisons</h2>
-                <p>See how you rank among your peers on popular platforms like Codeforces, LeetCode, and HackerRank. Compete and improve together!</p>
-            </div>
-        </div>
-    </section>
+        </section>
+    </>
   );
 }
 
